868f8955834197c05078971ff9088bfdc856bd7a
Augy <ateoks@gmail.com>
2026-09-25 03:52:51 +0800

    init struct

--- main-site/README.md
+++ main-site/README.md
@@ -1,3 +1,3 @@
-# pwa-template
-Augy Studios PWA sites template
+# pwa-wordle
+Augy Studios PWA sites wordle
 Note: The `/api` folder is meant for Vercel serverless functions. Remove if not required.
--- main-site/sw.js
+++ main-site/sw.js
@@ -1,12 +1,12 @@
-const CACHE = "template-offline-v1";
+const CACHE = "wordle-v1";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/templateicon1-192.png",
-  "/templateicon1-512.png",
+  "/XWD-192.png",
+  "/XWD-512.png",
   "/favicon.ico",
   "/manifest.json"
 ];
